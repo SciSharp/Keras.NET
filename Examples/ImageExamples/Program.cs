228ecacf7fd3ed898528b2ea7a189f1646b5a4f5
Deepak Battini <deepakkumar1984@gmail.com>
2019-08-15 12:32:43 +0930

    Fixed FitGenerator issue

--- Examples/ImageExamples/Program.cs
+++ Examples/ImageExamples/Program.cs
@@ -10,31 +10,6 @@ namespace ImageExamples
     {
         static void Main(string[] args)
         {
-            var model = new Sequential();
-            model.Add(new Conv2D(32, (3, 3).ToTuple(), input_shape: new Shape(64, 64, 3), activation: "relu"));
-
-            model.Add(new MaxPooling2D(pool_size: (2, 2).ToTuple()));
-
-            model.Add(new Flatten());
-
-            model.Add(new Dense(128, activation: "relu"));
-
-            model.Add(new Dense(1, activation: "sigmoid"));
-
-            model.Compile(optimizer: "adam", loss: "binary_crossentropy", metrics: new string[] { "accuracy" });
-
-
-            var datagen = new ImageDataGenerator(rescale: (float)1.0 / 255, shear_range: (float)0.2, zoom_range: (float)0.2, horizontal_flip: true);
-
-            //var train_datta = datagen.FlowFromDirectory("dataset/", class_mode:"binary", batch_size:64);
-
-            var train_dataset = datagen.FlowFromDirectory(directory: @"C:\Users\blitz\Downloads\kagglecatsanddogs_3367a\PetImages", target_size: (64, 64).ToTuple(), batch_size: 32, class_mode: "binary");
-
-
-            var test_dataset = datagen.FlowFromDirectory(directory: @"C:\Users\blitz\Downloads\kagglecatsanddogs_3367a\PetImages", target_size: (64, 64).ToTuple(), batch_size: 32, class_mode: "binary");
-
-            model.FitGenerator(train_dataset, steps_per_epoch: 8000, epochs: 25, validation_steps: 800, validation_data: test_dataset);
-
             //ImageRecognitionApplication.Run();
 
             //MNIST_CNN.Run();
